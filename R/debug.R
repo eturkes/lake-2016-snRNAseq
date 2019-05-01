b8e5911256b975cae293ce94e7e8fae579e25ecf
Emir Turkes <eturkes@bu.edu>
2019-05-01 15:14:27 -0400

    Update email

--- R/debug.R
+++ R/debug.R
@@ -14,6 +14,6 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-#    Emir Turkes can be contacted at eturkes@bu.edu
+#    Emir Turkes can be contacted at emir.turkes@eturkes.com
 
 devtools::install_github("qinzhu/PIVOT")
